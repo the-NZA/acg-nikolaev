4f55930a44594d91c1005c742ff4c808e16e892f
the_nza <the_nza@protonmail.com>
2021-07-21 16:20:14 +0300

    Add password hashing to user model and comparing passwords.

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-pkgz/lgr v0.10.4
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.7.0
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 )
--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -1,6 +1,15 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"github.com/the-NZA/acg-nikolaev/internal/app/helpers"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// hashCost for password hashing
+const hashCost = 15
 
 // User represets each user
 type User struct {
@@ -8,4 +17,30 @@ type User struct {
 	Username          string             `bson:"username" json:"username"`
 	EncryptedPassword string             `bson:"pswd" json:"-"`
 	Email             string             `bson:"email,omitempty" json:"email,omitempty"`
+}
+
+// Validate user struct
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.ID, validation.Required, validation.By(helpers.CheckObjectID)),
+		validation.Field(&u.Username, validation.Required, validation.RuneLength(8, 0)),
+		validation.Field(&u.EncryptedPassword, validation.Required),
+		validation.Field(&u.Email, validation.Required, is.EmailFormat),
+	)
+}
+
+// HashPassword generate password from input string
+func (u *User) HashPassword(pass string) error {
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(pass), hashCost)
+	if err != nil {
+		return err
+	}
+
+	u.EncryptedPassword = string(hashBytes)
+	return nil
+}
+
+// ComparePassword checks equality of given string and hashed passwords
+func (u User) ComparePassword(p string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.EncryptedPassword), []byte(p))
 }
