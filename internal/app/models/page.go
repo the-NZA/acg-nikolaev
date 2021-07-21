5c7344040c5099d3f9ea742437660ceb7b9f9d9c
the_nza <the_nza@protonmail.com>
2021-07-21 16:04:10 +0300

    Add validation to Page struct

--- internal/app/models/page.go
+++ internal/app/models/page.go
@@ -1,6 +1,10 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/the-NZA/acg-nikolaev/internal/app/helpers"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Page is basic model for each page
 type Page struct {
@@ -10,4 +14,16 @@ type Page struct {
 	MetaDesc string             `bson:"desc,omitempty" json:"desc,omitempty"`
 	Slug     string             `bson:"slug,omitempty" json:"slug,omitempty"`
 	PageData []Block            `bson:"pagedata,omitempty" json:"pagedata,omitempty"`
+}
+
+// Validate page struct
+func (p Page) Validate() error {
+	return validation.ValidateStruct(&p,
+		validation.Field(&p.ID, validation.Required, validation.By(helpers.CheckObjectID)),
+		validation.Field(&p.Title, validation.Required, validation.RuneLength(5, 50)),
+		validation.Field(&p.Subtitle, validation.Required, validation.RuneLength(30, 255)),
+		validation.Field(&p.MetaDesc, validation.Required, validation.RuneLength(50, 255)),
+		validation.Field(&p.Slug, validation.Required),
+		validation.Field(&p.PageData, validation.NilOrNotEmpty),
+	)
 }
